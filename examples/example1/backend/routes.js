396f374a4124c9b49313611be2cfa07cda2f4b45
hudaokeji <github@hudaokeji.com>
2018-01-03 01:53:37 +0900

    remove example1  to skynode-webap-example1

--- examples/example1/backend/routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-
-module.exports = function(app) {
-    app.get('/aboutme', function(req, res) {
-        res.redirect('http://www.hudaokeji.com');
-    });
-};
